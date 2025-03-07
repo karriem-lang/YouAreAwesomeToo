3fccf26769b5f2f649c5ebf4f729c8449a4b4015
karriem-lang <karriemlang@proton.me>
2025-03-07 04:57:37 -0500

    Initial Commit

---
+++ YouAreAwesomeToo/YouAreAwesomeTooApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeTooApp.swift
+//  YouAreAwesomeToo
+//
+//  Created by Karriem Lang on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeTooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
